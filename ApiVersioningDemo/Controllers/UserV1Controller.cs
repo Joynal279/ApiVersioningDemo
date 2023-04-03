fa7040c7021b2da1d58365f054688d9a642a5d5a
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-04-03 22:09:19 +0600

    initial commit

---
+++ ApiVersioningDemo/Controllers/UserV1Controller.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersioningDemo.Controllers
+{
+    [Route("api/user/[action]")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class UserV1Controller : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            return Ok("Get user data");
+        }
+    }
+}
