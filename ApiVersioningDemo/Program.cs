72740d7e8f451b2acb08606d6b67a1e8dc30e188
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-04-03 22:22:22 +0600

    unspecified version number and set hierarcy version number

--- ApiVersioningDemo/Program.cs
+++ ApiVersioningDemo/Program.cs
@@ -6,7 +6,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddApiVersioning();
+builder.Services.AddApiVersioning(option =>
+{
+    option.AssumeDefaultVersionWhenUnspecified = true;
+    option.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
